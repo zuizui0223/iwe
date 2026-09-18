2f6000824af70e3bed63fc1c33cbac5111c9d52f
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-18 17:55:41 +0900

    test: register log-odds effect family

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -20,4 +20,5 @@ def test_registered_effect_families_are_explicit():
         "standardized_mean_difference",
         "log_response_ratio",
         "log_rate_slope_per_day",
+        "log_odds_ratio",
     }
