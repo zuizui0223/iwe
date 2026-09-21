bb8e4065f5a890b671bfa0078af2ec62338536f1
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-21 10:07:06 +0900

    schema: execute timing metric contract

--- src/iwe/schema.py
+++ src/iwe/schema.py
@@ -15,6 +15,34 @@ PHENOLOGY_SOURCES = {
 
 EXPOSURE_DIRECTIONS = {"synchrony", "mismatch"}
 
+TIMING_METRIC_TYPES = {
+    "overlap_index",
+    "absolute_mismatch",
+    "plant_minus_partner",
+    "partner_minus_plant",
+    "experimental_plant_shift",
+    "seasonal_position",
+    "other_registered",
+}
+
+TIMING_ANALYSIS_CLASSES = {
+    "strict_window",
+    "direct_timing_sensitivity",
+    "directional_mismatch",
+    "unresolved_for_strict_h1",
+    "proxy_only",
+}
+
+TIMING_DOMAINS = {
+    "nonnegative",
+    "plant_earlier_only",
+    "partner_earlier_only",
+    "both_sides",
+    "ordered_by_measured_window",
+    "unknown",
+    "not_applicable",
+}
+
 EFFECT_FAMILIES = {
     "standardized_slope",
     "fisher_z",
@@ -32,6 +60,9 @@ REQUIRED_EFFECT_COLUMNS = [
     "interaction_type",
     "evidence_tier",
     "phenology_source",
+    "timing_metric_type",
+    "timing_analysis_class",
+    "timing_domain",
     "exposure_direction",
     "outcome_family",
     "effect_family",
