1b81637805e25a19a0efd3bfc67199b928a5f720
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-25 07:33:18 +0900

    Track unresolved timing linkage separately

--- src/iwe/replication_candidates.py
+++ src/iwe/replication_candidates.py
@@ -9,6 +9,7 @@ CANDIDATE_STATUSES = {
     "blocked_summary_stats",
     "blocked_effect_form",
     "blocked_final_surface",
+    "blocked_timing_linkage",
     "rejected_timing",
     "rejected_dependence",
     "rejected_other",
